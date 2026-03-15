c782af54b7fe27a4c51324ae37d1e13d63a73e0a
David Stone <david@nnucomputerwhiz.com>
2026-03-15 22:29:45 +0000

    chore: rebuild assets after rebase onto main

--- build/admin-page.asset.php
+++ build/admin-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '36c860c117b8ce685222');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3c24bad68c336231a901');
--- build/settings-page.asset.php
+++ build/settings-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '50e5711e8d976e74b4b0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4420dafdfd7f26e70f6e');

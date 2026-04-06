31382999cd86d1ceaea6bf357ff9e04ba1890338
David Stone <david@nnucomputerwhiz.com>
2026-04-05 23:28:00 -0600

    chore: rebuild assets

--- build/admin-page.asset.php
+++ build/admin-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ec5cf401302a04b13c23');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c72cde2e3955f961255e');
--- build/floating-widget.asset.php
+++ build/floating-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3ad89fcbb090fb9a4c7a');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'cef3cd69032c675ebf61');

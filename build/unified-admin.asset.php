5fef28512a7ba69ff93cf452050ca4d046837b5c
David Stone <david@nnucomputerwhiz.com>
2026-04-05 23:28:00 -0600

    chore: rebuild assets

--- build/unified-admin.asset.php
+++ build/unified-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7c3626cfe620a93229ec');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9ab9a4fee7428b25a61d');
